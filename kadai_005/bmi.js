f40f03c50e77d5bf4cd651a79bed7e8bd60f6e4d
mk-hoshino615 <mk.hoshino0402@gmail.com>
2026-02-18 16:59:11 +0900

    kadai_005　アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//体重（kg）
+let bodyWeight;
+
+//身長（m）
+let height;
+
+//ＢＭＩ
+let bmi;
+
+//変数の代入
+bodyWeight = 68;
+height = 1.7;
+
+bmi = bodyWeight / (height * height);
+
+//コンソールへの出力
+console.log(bmi);
